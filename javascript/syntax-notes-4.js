6b0527274925dd590038339c5f342f8da1ac22fa
paolomiguelcuento@gmail.com <paolomiguelcuento@gmail.com>
2026-08-25 23:16:43 +0800

    update

--- javascript/syntax-notes-4.js
+++ javascript/syntax-notes-4.js
@@ -1,7 +1,9 @@
 // -------------------
+// Display converted invoice base on rate api and its GBP outstanding balance
+// api source: https://frankfurter.dev/#rates
 const endpoint = `https://api.frankfurter.dev/v2/rates?${param()}`;
 
-async function index(endpoint) {
+async function fetchExchangeRate(endpoint) {
     const response = await fetch(endpoint);
 
     if (!response.ok) {
@@ -11,9 +13,40 @@ async function index(endpoint) {
     return await response.json();
 }
 
+
+function convertInvoiceAmountToGbp(data) {
+    const getInvoices = invoice();
+
+    return getInvoices.map(function (item) {
+        const filteredRate = data.filter((rates) => rates.quote === item.currency)[0].rate ?? 1;
+        const amount = parseFloat(item.amount);
+        item.rate = filteredRate;
+        item.converted = amount * filteredRate;
+        return item;
+    });
+}
+
+function calculateOutstandingGbpBalance(fetchConverted) {
+    let total = 0;
+    for (const item of fetchConverted){
+        total += item.converted;
+    }
+    return total;
+    // easy way const total = fetchConverted.reduce((sum, item) => sum + item.converted, 0);
+}
+
+function result(data) {
+    const fetchConverted = convertInvoiceAmountToGbp(data);
+    return {
+        conversion: fetchConverted,
+        outstanding: calculateOutstandingGbpBalance(fetchConverted)
+    }
+}
+
 function param() {
     const params = {
-        base: "GBP"
+        base: "GBP",
+        date: formattedDateToday()
     };
     return new URLSearchParams(params).toString();
 }
@@ -66,24 +99,19 @@ function invoice() {
     return data;
 }
 
-function displayExchangeRate() {
-    const getInvoices = invoice();
-    // return getInvoices.filter((item) => item.id === 1);
-    return getInvoices.map((item) => item.id === 1);
+function formattedDateToday() {
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
 }
 
-function computation(params) {
-    
-}
-
-console.log(displayExchangeRate());
-
-
-
-
-// index(endpoint)
-//     .then(data => console.log(data))
-//     .catch(error => console.error(error));
+fetchExchangeRate(endpoint)
+    .then(data => console.log(result(data)))
+    .catch(error => console.error(error));
 
 
 // This will cause cors error unless use local server
